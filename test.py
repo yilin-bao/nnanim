b6bb46acebd462c11d2661b77dc4b54753880076
yilin-bao <62961093+yilin-bao@users.noreply.github.com>
2023-12-08 23:54:28 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import hashlib
+import random
+import time
 import numpy as np
 import analyzer
 from TestingCode import vit
@@ -6,10 +9,26 @@ from TestingCode import modules
 # x = np.array([])
 # vit.VisionTransformer(hidden_dims=[100]).forward(x)
 
-la = analyzer.ModuleAnalyzer()
-la.start_analyze_module(vit.VisionTransformer(embedding_dim=2*768, hidden_dims=[100]))
+# la = analyzer.ModuleAnalyzer()
+# la.start_analyze_module(vit.VisionTransformer(embedding_dim=2*768, hidden_dims=[100]))
+# print(list(la.var_module_dict.keys()))
+# print(la.var_module_dict['transformer'])
 # la = analyzer.ModuleAnalyzer()
 # la.start_analyze_module(modules.Attention(dim=2*768))
+
+def generate_random_string(length=8):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def hash_code(code):
+    code = code + generate_random_string()
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(code.encode('utf-8'))
+    hashed_code = sha256_hash.hexdigest()
+    return hashed_code
+
+print(hash_code('x'))
+print(hash_code('x'))
 
 # print("="*40)
 
