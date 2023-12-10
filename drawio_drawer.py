6a94329b3fff8807342baf07213f10036dfdc59b
Yilin (Eleen) Bao <62961093+yilin-bao@users.noreply.github.com>
2023-12-09 18:42:28 -0600

    Update drawio_drawer.py

--- drawio_drawer.py
+++ drawio_drawer.py
@@ -1,26 +1,60 @@
+import ast
 import numpy as np
-import analyzer
+import analyzer as analyzer
 from TestingCode import vit
 from TestingCode import modules
 
+# la = analyzer.ModuleAnalyzer()
+# la.start_analyze_module(modules.Attention(dim=2*768))
 la = analyzer.ModuleAnalyzer()
-la.start_analyze_module(modules.Attention(dim=2*768))
+la.start_analyze_module(vit.VisionTransformer(embedding_dim=2*768, hidden_dims=[100]))
+
+# print(la.moudle_map)
+# print(la.hash_var_dict)
 
 from N2G import drawio_diagram
 
 diagram = drawio_diagram()
 diagram.add_diagram("Page-1")
 
-G_edge_form = []
+edge = []
 edge_labels = {}
-for m_map in la.moudle_map:
-    a = (m_map[0][0], m_map[1][0])
-    b = m_map[2]
-    a_1 = f"{m_map[0][0]}"
-    a_2 = f"{m_map[1][0]}"
-    diagram.add_node(id=a_1)
-    diagram.add_node(id=a_2)
-    diagram.add_link(a_1, a_2, label=str(b))
+for mm in la.moudle_map:
+    for a in mm[0]:
+        if a in list(la.hash_var_dict.keys()):
+            print(la.hash_var_dict[a])
+            diagram.add_node(label=la.hash_var_dict[a], id=a)
+        else:
+            diagram.add_node(id=a)
+    for b in mm[1]:
+        if b in list(la.hash_var_dict.keys()):
+            print(la.hash_var_dict[b])
+            diagram.add_node(label=la.hash_var_dict[b], id=b)
+        else:
+            diagram.add_node(id=b)
+    for a in mm[0]:
+        for b in mm[1]:
+            if mm[2] in list(la.var_module_dict.keys()):
+                arr = la.var_module_dict[mm[2]].split('.')
+                if len(arr) == 0:
+                    l = ""
+                elif len(arr) == 1:
+                    l = arr[0]
+                elif len(arr) > 1:
+                    l = arr[-1]
+                diagram.add_link(a, b, label=l)
+            else:
+                # print(mm[2])
+                if isinstance(mm[2], ast.Add): diagram.add_link(a, b, label='Add')
+                else: diagram.add_link(a, b, label=str(mm[2]))
+            
+    # a = (m_map[0][0], m_map[1][0])
+    # b = m_map[2]
+    # a_1 = f"{m_map[0][0]}"
+    # a_2 = f"{m_map[1][0]}"
+    # diagram.add_node(id=a_1)
+    # diagram.add_node(id=a_2)
+    # diagram.add_link(a_1, a_2, label=str(b))
 
 # diagram.add_node(id="R1")
 # diagram.add_node(id="R2")
